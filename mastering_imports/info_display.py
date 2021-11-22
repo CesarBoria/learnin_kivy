fb148ecd9e8986e51b3ac0a465c2141d310bfb31
CesarBoria <cesar510511@gmail.com>
2021-11-22 15:40:12 +0100

    The previous commit was the one in which I solved the problem of the lack of fluency in the updating of the info.

--- mastering_imports/info_display.py
+++ mastering_imports/info_display.py
@@ -1,12 +1,7 @@
-from functools import partial
-
-from kivy.app import App
-from kivy.clock import Clock
 from kivy.lang.builder import Builder
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 
-from mastering_imports.war_zone import WarZone
 
 Builder.load_string('''
 <Info>:
@@ -30,15 +25,3 @@ Builder.load_string('''
 class Info(Widget):
     # killed = StringProperty()
     to_kill = StringProperty('10')
-
-'''
-class MyApp(App):
-    def build(self):
-        ob = Info()
-        return ob
-
-
-if __name__ == '__main__':
-    app_instance = MyApp()
-    app_instance.run()
-'''
--- mastering_imports/war_zone.py
+++ mastering_imports/war_zone.py
@@ -1,8 +1,7 @@
-from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 
-from mastering_imports.objects import Sum, Sub, InputArea, Gun, GunChulo, Enemy
+from mastering_imports.objects import Gun, GunChulo, Enemy
 
 
 class WarZone(Widget):
@@ -17,8 +16,8 @@ class WarZone(Widget):
         super().__init__(**kwargs)
         self.info = info
         self.add_widget(self.info)
-        self.a = Clock.schedule_interval(self.update, 1 / 60)
-        self.b = Clock.schedule_interval(self.spawn_enemy_simple, 2)
+        self.update_event = Clock.schedule_interval(self.update, 1 / 60)
+        self.spawn_enemy_event = Clock.schedule_interval(self.spawn_enemy_simple, 2)
         Clock.schedule_interval(self.spawn_enemy_sum, 6)
         Clock.schedule_interval(self.spawn_enemy_sub, 7)
 
@@ -60,14 +59,14 @@ class WarZone(Widget):
     def game_over_routine(self):
         for enemy in self.enemies:
             if enemy.check_conquest():
-                self.a.cancel()
-                self.b.cancel()
+                self.update_event.cancel()
+                self.spawn_enemy_event.cancel()
                 print('GAME OVER')
 
     def win_routine(self):
         if self.killed_enemies == self.num_enemies:
-            self.a.cancel()
-            self.b.cancel()
+            self.update_event.cancel()
+            self.spawn_enemy_event.cancel()
             print('WIN!')
 
     def update(self, dt):
@@ -85,120 +84,3 @@ class WarZone(Widget):
                     self.kill_enemy(enemy=enemy)
         for bullet in self.bullets:
             bullet.move('dt')
-
-'''
-if __name__ == '__main__':
-    class GunAdministrator(Widget):
-        guns = []
-
-        def __init__(self, GUI, **kwargs):
-            super().__init__(**kwargs)
-            self.GUI = GUI
-            gun1 = Sum(self.GUI)
-            gun2 = Sub(self.GUI)
-            gun2.pos = 200, 0
-            gun2.size = 200, 200
-            gun1.pos = 0, 0
-            gun1.size = 200, 200
-            self.add_widget(gun1)
-            self.add_widget(gun2)
-            self.guns.append(gun1)
-            self.guns.append(gun2)
-            self.add_widget(InputArea())
-
-        def all_off(self):
-            for gun in self.guns:
-                gun.state = 'OFF'
-                if gun.result != gun.response:
-                    gun.response = ''
-
-        def button_clicked(self, gun):
-            self.all_off()
-            gun.state = 'ON'
-            self.update_colors()
-            if gun.color == [0, 1, 0, 1]:
-                gun.shoot()
-
-        def update_colors(self):
-            for gun in self.guns:
-                if gun.response == gun.result:
-                    gun.color = [0, 1, 0, 1]
-                elif gun.state == 'ON' and gun.response != '':
-                    gun.color = [1, 0, 0, 1]
-                elif gun.state == 'ON' and gun.response == '':
-                    gun.color = [0, 0, 1, 1]
-                else:
-                    gun.color = [1, 1, 1, 1]
-
-        def type_number(self, text):
-            print(text)
-            for gun in self.guns:
-                if gun.state == 'ON':
-                    gun.response += text
-            self.update_colors()
-
-
-    class WarZone(Widget):
-        enemies = []
-        bullets = Gun.bullets
-        bullets_Sum = GunChulo.bullets
-
-        def spawn_enemy_simple(self, dt):
-            enemy = Enemy(0)
-            self.enemies.append(enemy)
-            self.add_widget(enemy)
-
-        def spawn_enemy_sum(self, dt):
-            enemy = Enemy(150)
-            self.enemies.append(enemy)
-            self.add_widget(enemy)
-
-        def spawn_enemy_sub(self, dt):
-            enemy = Enemy(350)
-            self.enemies.append(enemy)
-            self.add_widget(enemy)
-
-        def move_enemies(self):
-            for enemy in self.enemies:
-                enemy.move()
-
-        def kill_enemy(self, enemy):
-            self.remove_widget(enemy)
-            self.enemies.remove(enemy)
-
-        def kill_bullet(self, bullet):
-            self.bullets.remove(bullet)  # This one only works with the simple gun.
-            self.remove_widget(bullet)
-
-        def update(self, dt):
-            self.move_enemies()
-            for enemy in self.enemies:
-                for bullet in self.bullets:
-                    if enemy.check_collision(bullet):
-                        self.kill_bullet(bullet=bullet)
-                        self.kill_enemy(enemy=enemy)
-                for bullet in self.bullets_Sum:
-                    if enemy.check_collision(bullet):
-                        self.kill_bullet(bullet=bullet)
-                        self.kill_enemy(enemy=enemy)
-                if enemy.check_conquest():
-                    pass  # TODO: Run Game Over Routine.
-            for bullet in self.bullets:
-                bullet.move('dt')
-
-
-    class MyApp(App):
-        def build(self):
-            ob = WarZone()
-            ob.add_widget(Gun(ob))
-            ob.add_widget(GunAdministrator(ob))
-            Clock.schedule_interval(ob.update, 1 / 60)
-            Clock.schedule_interval(ob.spawn_enemy_simple, 2)
-            Clock.schedule_interval(ob.spawn_enemy_sum, 6)
-            Clock.schedule_interval(ob.spawn_enemy_sub, 7)
-            return ob
-
-
-    app_instance = MyApp()
-    app_instance.run()
-'''
